53a3665d224f93c064f21bf95a954e9a5aa03c9c
Destiney Rodney <destiney.rodney01@gmail.com>
2022-05-24 22:21:28 -0500

    feat: Adding database

--- src/main/java/com/example/newlywed/NewlyWedApplication.java
+++ src/main/java/com/example/newlywed/NewlyWedApplication.java
@@ -1,13 +1,30 @@
 package com.example.newlywed;
 
+import com.example.newlywed.models.User;
+import com.example.newlywed.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NewlyWedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NewlyWedApplication.class, args);
+
+    }
+
+    @Bean
+    public CommandLineRunner run(UserRepository repository){
+        return (args ->{
+            insertJavaAdvocates(repository);
+            System.out.println(repository.findAll());
+        });
+    }
+
+    private void insertJavaAdvocates(UserRepository repository){
+        repository.save(new User());
     }
 
 }
--- src/main/java/com/example/newlywed/repositories/UserRepository.java
+++ src/main/java/com/example/newlywed/repositories/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//to perform crud
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
    User findByUsername(String username);
